8e68e652bd600682adcfb854710f47a6ebb764eb
CamiloRiosBaena <camiloriosba@gmail.com>
2024-10-30 17:08:44 -0500

    Se añadio la opcion de perder y ganar vida

--- game.cpp
+++ game.cpp
@@ -5,11 +5,19 @@
 
 using namespace std;
 
+struct Player{
+    int lives=2;
+    int health=100;
+};
+
+Player player_s;
+
 void win(int i, int j, char movement, char game[][20]){
     if((movement=='R' && j<19 && game[i][j+1]=='C')||
        (movement=='L' && j>0 && game[i][j-1]=='C')||
        (movement=='U' && i>0 && game[i-1][j]=='C')||
        (movement=='D' && i<9 && game[i+1][j]=='C')){
+            system("cls");
              cout<<"############\n";
              cout<<"# You win #\n";
              cout<<"############\n";
@@ -20,18 +28,74 @@ void win(int i, int j, char movement, char game[][20]){
        }
 }
 
-void lose(int i, int j, char movement, char game[][20]){
+void win_hp(int i, int j, char movement, char game[][20]){
+    if(player_s.health<100 && ((movement=='R' && j<19 && game[i][j+1]=='H')||
+       (movement=='L' && j>0 && game[i][j-1]=='H')||
+       (movement=='U' && i>0 && game[i-1][j]=='H')||
+       (movement=='D' && i<9 && game[i+1][j]=='H'))){
+
+            player_s.health+=50;
+
+            system("cls");
+             cout<<"##################\n";
+             cout<<"# You won 50 Hp  #\n";
+             cout<<"##################\n";
+             cout<<"Press enter to continue...";
+             cin.ignore();
+             getchar();
+             system("cls");
+
+             cout<<"###########################\n";
+             cout<<"# Your current Hp is "<<player_s.health<<" #\n";
+             cout<<"###########################\n";
+             cout<<"Press enter to continue...";
+             getchar();
+       }
+}
+
+void lose_hp(int i, int j, char movement, char game[][20]){
     if((movement=='R' && j<19 && game[i][j+1]=='M')||
        (movement=='L' && j>0 && game[i][j-1]=='M')||
        (movement=='U' && i>0 && game[i-1][j]=='M')||
        (movement=='D' && i<9 && game[i+1][j]=='M')){
-             cout<<"############\n";
-             cout<<"# You lose #\n";
-             cout<<"############\n";
-             cout<<"Presione enter para salir...";
-             cin.ignore();
-             getchar();
-             exit(0);
+
+            player_s.health-=50;
+
+            if(player_s.health>0){
+
+                system("cls");
+                cout<<"###################\n";
+                cout<<"# You lost 50 Hp  #\n";
+                cout<<"###################\n";
+                cout<<"Press enter to continue...";
+                cin.ignore();
+                getchar();
+                system("cls");
+
+                cout<<"##########################\n";
+                cout<<"# Your current Hp is: "<<player_s.health<<" #\n";
+                cout<<"##########################\n";
+                getchar(); 
+
+            } 
+            else {
+                player_s.lives-=1;
+                player_s.health=100;
+
+                system("cls");
+                cout << "######################################\n";
+                cout << "# You've lost one life. " << player_s.lives << " remaining #\n";
+                cout << "######################################\n";
+                cout << "Press enter to continue...";
+                cin.ignore();
+                getchar();
+                system("cls");
+
+                cout << "##################################\n";
+                cout << "# Your health has been restored  #\n";
+                cout << "##################################\n";
+                getchar();
+            }
        }
 }
 
@@ -40,8 +104,8 @@ void mov(char movement, char game[][20]){
     for(i=0;i<10;i++){
         for(j=0;j<20;j++){
             if(game[i][j]=='P'){
-
-                lose(i,j,movement, game);
+                lose_hp(i,j,movement, game);
+                win_hp(i,j,movement,game);
                 win(i,j,movement, game);
 
                 game[i][j]=' ';
@@ -69,7 +133,7 @@ void pos(char entity, int cnt, char game[][20]){
     }
 }
 
-void game_f(char mon, char chest, char player, char game[][20]){
+void game_f(char mon, char chest, char player, char potion, char game[][20]){
     int i, f;
 
     for(i=0; i<10; i++){
@@ -82,6 +146,7 @@ void game_f(char mon, char chest, char player, char game[][20]){
     pos(mon, 3, game);
     pos(chest, 1, game);
     pos(player, 1, game);
+    pos(potion, 2, game);
 }
 
 void print(char game[][20]){
@@ -100,10 +165,11 @@ int main(){
     char Monster = 'M';
     char Chest = 'C';
     char Player = 'P';
+    char Potion = 'H';
 
     srand(time(0));
 
-    game_f(Monster, Chest, Player, game);
+    game_f(Monster, Chest, Player, Potion, game);
 
     do{
         print(game);
